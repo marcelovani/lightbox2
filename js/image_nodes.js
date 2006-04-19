98f1984468f15b8399b65c586b55a2f3e39bdc78
Steve McKenzie <steve_mckenzie@45890.no-reply.drupal.org>
2006-04-19 07:03:02 +0000

    fixed image node autosetup

--- js/image_nodes.js
+++ js/image_nodes.js
@@ -1,11 +1,12 @@
 function lightbox2_image_nodes() {
-	var nodes = document.getElementsByClassName("image thumbnail");
-	
+	var nodes = document.getElementsByClassName("image");
 	for (var i = 0; i < nodes.length; i++) {
-		var parent = nodes[i].parentNode;
-		parent.rel = "lightbox[node_thumbnails]";
-		parent.href = nodes[i].src.replace('.thumbnail', '');
-		parent.title = nodes[i].alt;
+		if (Element.hasClassName(nodes[i], "thumbnail")) {
+			var parent = nodes[i].parentNode;
+			parent.rel = "lightbox[node_thumbnails]";
+			parent.href = nodes[i].src.replace(".thumbnail", "");
+			parent.title = nodes[i].alt;
+		}
 	}
 }
 
