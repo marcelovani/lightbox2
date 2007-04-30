47643a47f6626a2ff8ffd7cf3d0707b76a381ed7
Stella Power <stella@66894.no-reply.drupal.org>
2007-04-30 10:40:12 +0000

    added a try {} on the addLoadEvent and also updated the css div name for the image thumbnail from "thumbnail" to "image-thumbnail". Added "view image information" link to lightbox

--- js/image_nodes_nogroup.js
+++ js/image_nodes_nogroup.js
@@ -4,15 +4,22 @@
 function lightbox2_image_nodes() {
 	var nodes = document.getElementsByClassName("image");
 	for (var i = 0; i < nodes.length; i++) {
-		if (Element.hasClassName(nodes[i], "thumbnail")) {
+		if (Element.hasClassName(nodes[i], "image-thumbnail")) {
 			var parent = nodes[i].parentNode;
 			parent.rel = "lightbox";
+			parent.title = nodes[i].alt.concat("<br /><br /><a href=\"" + parent.href + "\">View Image Information</a>");
 			parent.href = nodes[i].src.replace(".thumbnail", "");
-			parent.title = nodes[i].alt;
+			//parent.title = nodes[i].alt;
 		}
 	}
 }
 
-if (isJsEnabled()) {
-  addLoadEvent(lightbox2_image_nodes);
+try {
+		if (isJsEnabled()) {
+				addLoadEvent(lightbox2_image_nodes);
+		}
+} catch (e) {
+		if (Drupal.jsEnabled) {
+				$(document).ready(lightbox2_image_nodes);
+		}
 }
