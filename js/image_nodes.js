38543a6001fdeb66aa5e2fa75cb37dada1a73386
Stella Power <stella@66894.no-reply.drupal.org>
2007-05-03 11:43:59 +0000

    added in fix for #140845 as different versions of the image gallery module have different class names.  Also added in a fix for #140463 (javascript error) and feature request #71443 so there is now a link to the image node from the lightbox.

--- js/image_nodes.js
+++ js/image_nodes.js
@@ -1,18 +1,17 @@
 // Image Node Auto-Format with Auto Image Grouping
 // Steve McKenzie
 
-function lightbox2_image_nodes() {
-	var nodes = document.getElementsByClassName("image");
-	for (var i = 0; i < nodes.length; i++) {
-		if (Element.hasClassName(nodes[i], "thumbnail")) {
-			var parent = nodes[i].parentNode;
-			parent.rel = "lightbox[node_thumbnails]";
-			parent.href = nodes[i].src.replace(".thumbnail", "");
-			parent.title = nodes[i].alt;
-		}
-	}
-}
-
-if (isJsEnabled()) {
-  addLoadEvent(lightbox2_image_nodes);
+if (Drupal.jsEnabled) {
+		$(document).ready(function lightbox2_image_nodes() {
+				var nodes = document.getElementsByClassName("image");
+				for (var i = 0; i < nodes.length; i++) {
+				 	if (Element.hasClassName(nodes[i], "image-thumbnail") || Element.hasClassName(nodes[i], "thumbnail")) {
+				  		var parent = nodes[i].parentNode;
+				  		parent.rel = "lightbox[node_thumbnails]";
+				  		parent.title = nodes[i].alt.concat("<br /><br /><a href=\"" + parent.href + "\">View Image Information</a>");
+				  		parent.href = nodes[i].src.replace(".thumbnail", "");
+				  		//parent.title = nodes[i].alt;
+				 	}
+		 	}
+		});
 }
