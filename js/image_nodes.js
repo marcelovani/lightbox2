18b8de7c69dc8c0c5c078b6f0283e29b6cc1d517
Stella Power <stella@66894.no-reply.drupal.org>
2007-05-03 10:56:32 +0000

    added in patch for #71443 - there's now a link to the image node from the lightbox

--- js/image_nodes.js
+++ js/image_nodes.js
@@ -7,8 +7,9 @@ function lightbox2_image_nodes() {
 		if (Element.hasClassName(nodes[i], "thumbnail")) {
 			var parent = nodes[i].parentNode;
 			parent.rel = "lightbox[node_thumbnails]";
+			parent.title = nodes[i].alt.concat("<br /><br /><a href=\"" + parent.href + "\">View Image Information</a>");
 			parent.href = nodes[i].src.replace(".thumbnail", "");
-			parent.title = nodes[i].alt;
+			//parent.title = nodes[i].alt;
 		}
 	}
 }
