536630d8427cdee8f0b01b7b0069a1fd670627dc
Stella Power <stella@66894.no-reply.drupal.org>
2007-05-03 13:09:39 +0000

    changes for #86408 to allow users to disable the lightbox for gallery lists, but have it still work for images within the gallery.

---
+++ js/image_nodes_galleries.js
@@ -0,0 +1,33 @@
+// Image Node Auto-Format with Auto Image Grouping and no lightbox for nested
+// gallery links
+// Stella Power
+
+if (Drupal.jsEnabled) {
+		$(document).ready(function lightbox2_image_nodes() {
+				var gallery = new Array();
+				var nodes = document.getElementsByClassName("galleries");
+				for (var i = 0; i < nodes.length; i++) {
+						// select all the images inside those lists
+						var sub = nodes[i].getElementsByTagName("img");
+						for (var j = 0; j < sub.length; j++) {
+								if (Element.hasClassName(sub[j], "image-thumbnail") || Element.hasClassName(sub[j], "thumbnail")) {
+										gallery.push(sub[j]);
+								}
+						}
+				}
+
+
+				var nodes = document.getElementsByClassName("image");
+				for (var i = 0; i < nodes.length; i++) {
+						if (Element.hasClassName(nodes[i], "image-thumbnail") || Element.hasClassName(nodes[i], "thumbnail")) {
+								if (gallery.indexOf(nodes[i]) == -1) {
+										var parent = nodes[i].parentNode;
+										parent.rel = "lightbox[node_thumbnails]";
+										parent.title = nodes[i].alt.concat("<br /><br /><a href=\"" + parent.href + "\">View Image Information</a>");
+										parent.href = nodes[i].src.replace(".thumbnail", "");
+										//parent.title = nodes[i].alt;
+								}
+						}
+				}
+		});
+}
