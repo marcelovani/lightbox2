b6ec355966592be562b6624ba1c344afedc21ddf
Stella Power <stella@66894.no-reply.drupal.org>
2007-05-16 15:26:54 +0000

    fixed issue reported in #144513 - img_assist support

--- js/image_nodes_nogroup.js
+++ js/image_nodes_nogroup.js
@@ -3,14 +3,17 @@
 
 if (Drupal.jsEnabled) {
 		$(document).ready(function lightbox2_image_nodes() {
-				var nodes = document.getElementsByClassName("image");
-				for (var i = 0; i < nodes.length; i++) {
-				 	if (Element.hasClassName(nodes[i], "image-thumbnail") || Element.hasClassName(nodes[i], "thumbnail")) {
-						 	var parent = nodes[i].parentNode;
-						 	parent.rel = "lightbox";
-						 	parent.title = nodes[i].alt.concat("<br /><br /><a href=\"" + parent.href + "\">View Image Information</a>");
-						 	parent.href = nodes[i].src.replace(".thumbnail", "");
-						 	//parent.title = nodes[i].alt;
+				var body = document.getElementsByTagName("body");
+				if (!Element.hasClassName(body, "img_assist")) {
+						var nodes = document.getElementsByClassName("image");
+						for (var i = 0; i < nodes.length; i++) {
+								if (Element.hasClassName(nodes[i], "image-thumbnail") || Element.hasClassName(nodes[i], "thumbnail")) {
+										var parent = nodes[i].parentNode;
+										parent.rel = "lightbox";
+										parent.title = nodes[i].alt.concat("<br /><br /><a href=\"" + parent.href + "\">View Image Information</a>");
+										parent.href = nodes[i].src.replace(".thumbnail", "");
+										//parent.title = nodes[i].alt;
+								}
 						}
 				}
 		});
