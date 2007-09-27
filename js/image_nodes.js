c07a45979266f660a033da0fc75840ee07cd0a6f
Stella Power <stella@66894.no-reply.drupal.org>
2007-09-27 15:24:42 +0000

    added in support for Inline module

--- js/image_nodes.js
+++ js/image_nodes.js
@@ -3,21 +3,21 @@
 // Altered by Stella Power for jQuery version
 
 if (Drupal.jsEnabled) {
-		$(document).ready(function lightbox2_image_nodes() {
+  $(document).ready(function lightbox2_image_nodes() {
 
-				var img_assist = document.getElementById("img_assist_thumbs");
-				if (!img_assist) {
-						$("a[img.image-thumbnail, img.thumbnail]").each(function(i) {
+    var img_assist = document.getElementById("img_assist_thumbs");
+    if (!img_assist) {
+      $("a[img.inline, img.image-thumbnail, img.thumbnail]").each(function(i) {
 
-								var child = $(this).children();
-								var alt = $(child).attr("alt");
-								var link_text = "View Image Information";
-								$(this).attr({rel: "lightbox[node_thumbnails]",
-										title: alt + "<br /><br /><a href=\"" + this.href + "\">"+ link_text + "</a>",
-										href: $(child).attr("src").replace(".thumbnail", ""),
-										});
+        var child = $(this).children();
+        var alt = $(child).attr("alt");
+        var link_text = "View Image Information";
+        $(this).attr({rel: "lightbox[node_thumbnails]",
+          title: alt + "<br /><br /><a href=\"" + this.href + "\">"+ link_text + "</a>",
+          href: $(child).attr("src").replace(".thumbnail", ""),
+          });
 
-						});
-				}
-		});
+      });
+    }
+  });
 }
