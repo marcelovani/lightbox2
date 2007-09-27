9c0a1c13c86d04fce4b0f84481b95fd6bc391038
Stella Power <stella@66894.no-reply.drupal.org>
2007-09-27 19:42:47 +0000

    fix for #179201 - problem in IE

--- js/image_nodes_galleries.js
+++ js/image_nodes_galleries.js
@@ -3,21 +3,21 @@
 // Stella Power
 
 if (Drupal.jsEnabled) {
-		$(document).ready(function lightbox2_image_nodes() {
+  $(document).ready(function lightbox2_image_nodes() {
    var img_assist = document.getElementById("img_assist_thumbs");
     if (!img_assist) {
-						// apply to all other images
+      // apply to all other images
       $("a[img.inline, img.image-thumbnail, img.thumbnail]").each(function(i) {
-								if (!$(this).parents(".galleries").length ) {
+        if (!$(this).parents(".galleries").length ) {
 
         var child = $(this).children();
         var alt = $(child).attr("alt");
         var link_text = "View Image Information";
         $(this).attr({rel: "lightbox[node_thumbnails]",
           title: alt + "<br /><br /><a href=\"" + this.href + "\">"+ link_text + "</a>",
-          href: $(child).attr("src").replace(".thumbnail", ""),
+          href: $(child).attr("src").replace(".thumbnail", "")
           });
-								}
+        }
 
       });
     }
