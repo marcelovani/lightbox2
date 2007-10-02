0d74257814be2dd4fc9c875e50f30bb9c0d51e31
Stella Power <stella@66894.no-reply.drupal.org>
2007-10-02 16:14:55 +0000

    altered disable / enable form elements cos of new form elements added

--- js/lightbox2.js
+++ js/lightbox2.js
@@ -14,11 +14,13 @@ function lightbox2_lite_handler(event) {
   // enable / disable the image node options
   if ($("input[@name=lightbox2_lite]:checked").val() == 1) {
     $("input[@name=lightbox2_image_node]").attr("disabled", "disabled");
+    $("input[@name=lightbox2_node_link_text]").attr("disabled", "disabled");
     $("input[@name=lightbox2_image_group]").attr("disabled", "disabled");
     $("input[@name=lightbox2_disable_nested_galleries]").attr("disabled", "disabled");
   }
   else {
     $("input[@name=lightbox2_image_node]").removeAttr("disabled");
+    $("input[@name=lightbox2_node_link_text]").removeAttr("disabled");
     $("input[@name=lightbox2_image_group]").removeAttr("disabled");
     $("input[@name=lightbox2_disable_nested_galleries]").removeAttr("disabled");
     image_node_handler();
@@ -28,10 +30,12 @@ function lightbox2_lite_handler(event) {
 function image_node_handler(event) {
   if ($("input[@name=lightbox2_lite]:checked").val() != 1) {
     if ($("input[@name=lightbox2_image_node]:checked").val() == 1) {
+      $("input[@name=lightbox2_node_link_text]").removeAttr("disabled");
       $("input[@name=lightbox2_image_group]").removeAttr("disabled");
       $("input[@name=lightbox2_disable_nested_galleries]").removeAttr("disabled");
     }
     else {
+      $("input[@name=lightbox2_node_link_text]").attr("disabled", "disabled");
       $("input[@name=lightbox2_image_group]").attr("disabled", "disabled");
       $("input[@name=lightbox2_disable_nested_galleries]").attr("disabled", "disabled");
     }
