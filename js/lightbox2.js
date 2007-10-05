23f39a13c040fe66aef17b305c905426ea3efa5f
Stella Power <stella@66894.no-reply.drupal.org>
2007-10-05 14:18:49 +0000

    added in handling of new form elements

--- js/lightbox2.js
+++ js/lightbox2.js
@@ -5,6 +5,7 @@ if (Drupal.jsEnabled) {
     lightbox2_lite_handler();
     image_node_handler();
     $("input[@name=lightbox2_lite]").bind("click", lightbox2_lite_handler);
+    $("input[@name=lightbox2_use_alt_layout]").bind("click", alt_layout_handler);
     $("input[@name=lightbox2_image_node]").bind("click", image_node_handler);
   });
 }
@@ -13,6 +14,8 @@ if (Drupal.jsEnabled) {
 function lightbox2_lite_handler(event) {
   // enable / disable the image node options
   if ($("input[@name=lightbox2_lite]:checked").val() == 1) {
+    $("input[@name=lightbox2_use_alt_layout]").attr("disabled", "disabled");
+    $("input[@name=lightbox2_force_show_nav]").attr("disabled", "disabled");
     $("input[@name=lightbox2_disable_zoom]").attr("disabled", "disabled");
     $("input[@name=lightbox2_image_node]").attr("disabled", "disabled");
     $("input[@name=lightbox2_node_link_text]").attr("disabled", "disabled");
@@ -21,6 +24,8 @@ function lightbox2_lite_handler(event) {
     $("input[@name=lightbox2_image_count_str]").attr("disabled", "disabled");
   }
   else {
+    $("input[@name=lightbox2_use_alt_layout]").removeAttr("disabled");
+    $("input[@name=lightbox2_force_show_nav]").removeAttr("disabled");
     $("input[@name=lightbox2_disable_zoom]").removeAttr("disabled");
     $("input[@name=lightbox2_image_node]").removeAttr("disabled");
     $("input[@name=lightbox2_node_link_text]").removeAttr("disabled");
@@ -28,9 +33,20 @@ function lightbox2_lite_handler(event) {
     $("input[@name=lightbox2_disable_nested_galleries]").removeAttr("disabled");
     $("input[@name=lightbox2_image_count_str]").removeAttr("disabled");
     image_node_handler();
+    alt_layout_handler();
   }
 }
 
+function alt_layout_handler(event) {
+  if ($("input[@name=lightbox2_lite]:checked").val() != 1) {
+    if ($("input[@name=lightbox2_use_alt_layout]:checked").val() == 1) {
+      $("input[@name=lightbox2_force_show_nav]").attr("disabled", "disabled");
+    }
+    else {
+      $("input[@name=lightbox2_force_show_nav]").removeAttr("disabled");
+    }
+  }
+}
 function image_node_handler(event) {
   if ($("input[@name=lightbox2_lite]:checked").val() != 1) {
     if ($("input[@name=lightbox2_image_node]:checked").val() == 1) {
