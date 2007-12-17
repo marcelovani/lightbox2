a77cba7ead8b907e85a7958f0be7e2dd2bd784fb
Stella Power <stella@66894.no-reply.drupal.org>
2007-12-17 11:56:24 +0000

    changed js file to use new Drupal behaviours method of attaching javascript functionality.  Also fixed an issue caused by a change in jQuery selectors.

--- js/image_nodes.js
+++ js/image_nodes.js
@@ -2,57 +2,60 @@
 // Original version by Steve McKenzie
 // Altered by Stella Power for jQuery version
 
-if (Drupal.jsEnabled) {
-  $(document).ready(function lightbox2_image_nodes() {
+Drupal.behaviors.initAutoLightbox = function (context) { 
+  lightbox2_image_nodes();
+};
 
-    var settings = Drupal.settings.lightbox2;
+function lightbox2_image_nodes() {
 
-    // don't do it on the image assist popup selection screen
-    var img_assist = document.getElementById("img_assist_thumbs");
-    if (!img_assist) {
+  var settings = Drupal.settings.lightbox2;
 
-      // select the enabled image types
-      var classes = settings.image_node_classes;
-      $("a["+classes+"]").each(function(i) {
+  // don't do it on the image assist popup selection screen
+  var img_assist = document.getElementById("img_assist_thumbs");
+  if (!img_assist) {
 
-        if (!settings.disable_for_gallery_lists || (settings.disable_for_gallery_lists && !$(this).parents(".galleries").length)) {
-          var child = $(this).children();
+    // select the enabled image types
+    var classes = settings.image_node_classes;
+    $("a:has("+classes+")").each(function(i) {
 
-          // set the alt text
-          var alt = $(child).attr("alt");
-          if (!alt) {
-            alt = "";
-          }
+      if (!settings.disable_for_gallery_lists || (settings.disable_for_gallery_lists && !$(this).parents(".galleries").length)) {
+        var child = $(this).children();
 
-          // set the image node link text
-          var link_text = settings.node_link_text;
-
-          // set the rel attribute
-          var rel = "lightbox";
-          if (settings.group_images) {
-            rel = "lightbox[node_thumbnails]";
-          }
-
-          // set the href attribute
-          var href = $(child).attr("src").replace(".thumbnail", "").replace(/(image\/view\/\d+)(\/\w*)/, "$1/_original");
-
-          // handle flickr images
-          if ($(child).attr("class").match("flickr-photo-img")) {
-            href = $(child).attr("src").replace("_s", "").replace("_t", "").replace("_m", "").replace("_b", "");
-            if (settings.group_images) {
-              rel = "lightbox[flickr]";
-            }
-          }
-
-          // modify the image url
-          $(this).attr({rel: rel,
-            title: alt + "<br /><a href=\"" + this.href + "\" id=\"node_link_text\">"+ link_text + "</a>",
-            href: href
-            });
+        // set the alt text
+        var alt = $(child).attr("alt");
+        if (!alt) {
+          alt = "";
         }
 
-      });
+        // set the image node link text
+        var link_text = settings.node_link_text;
 
-    }
-  });
+        // set the rel attribute
+        var rel = "lightbox";
+        if (settings.group_images) {
+          rel = "lightbox[node_thumbnails]";
+        }
+
+        // set the href attribute
+        var href = $(child).attr("src").replace(".thumbnail", "").replace(/(image\/view\/\d+)(\/\w*)/, "$1/_original");
+
+        // handle flickr images
+        if ($(child).attr("class").match("flickr-photo-img")) {
+          href = $(child).attr("src").replace("_s", "").replace("_t", "").replace("_m", "").replace("_b", "");
+          if (settings.group_images) {
+            rel = "lightbox[flickr]";
+          }
+        }
+
+        // modify the image url
+        $(this).attr({
+          title: alt + "<br /><a href=\"" + this.href + "\" id=\"node_link_text\">"+ link_text + "</a>",
+          rel: rel,
+          href: href
+          });
+      }
+
+    });
+
+  }
 }
