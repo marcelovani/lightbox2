b36976358baca1373cdf70f418154d45112c41f4
Stella Power <stella@66894.no-reply.drupal.org>
2008-05-09 10:47:07 +0000

    Tidied up js code (ran through jsLint)

--- js/auto_image_handling.js
+++ js/auto_image_handling.js
@@ -4,6 +4,140 @@
 // Original version by Steve McKenzie.
 // Altered by Stella Power for jQuery version.
 
+function parse_url(url, param) {
+  param = param.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+  url = url.replace(/&amp;/, "&");
+  var regexS = "[\\?&]"+param+"=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(url);
+  if (results === null) {
+    return "";
+  }
+  else {
+    return results[1];
+  }
+}
+
+
+function lightbox2_init_triggers(classes, rel_type, custom_class) {
+  var settings = Drupal.settings.lightbox2;
+
+  $("a["+classes+"]").each(function(i) {
+
+    if ((!settings.disable_for_gallery_lists && !settings.disable_for_acidfree_gallery_lists) || (!$(this).parents(".galleries").length && !$(this).parents(".acidfree-folder").length && !$(this).parents(".acidfree-list").length) || ($(this).parents(".galleries").length && !settings.disable_for_gallery_lists) || (($(this).parents(".acidfree-folder").length || $(this).parents(".acidfree-list").length) && !settings.disable_for_acidfree_gallery_lists)) {
+
+      var child = $(this).children(classes);
+
+      // Ensure the child has a class attribute we can work with.
+      if ($(child).attr("class")) {
+
+        // Set the alt text.
+        var alt = $(child).attr("alt");
+        if (!alt) {
+          alt = "";
+        }
+
+        // Set the image node link text.
+        var link_text = settings.node_link_text;
+        var link_target  = "";
+        if (settings.node_link_target !== 0) {
+          link_target = 'target="'+ settings.node_link_target +'"';
+        }
+
+        // Set the rel attribute.
+        var rel = "lightbox";
+        if (rel_type != "lightbox_ungrouped") {
+          rel = rel_type + "[" + $(child).attr("class") + "]";
+        }
+
+        // Set the basic href attribute - need to ensure there's no language
+        // string (e.g. /en) prepended to the URL.
+        var id = null;
+        var href = $(child).attr("src");
+        var orig_href = $(this).attr("href");
+        var pattern = new RegExp(settings.file_path);
+        if (orig_href.match(pattern)) {
+          var lang_pattern = new RegExp(settings.base_path + "\\w\\w\\/");
+          orig_href = orig_href.replace(lang_pattern, settings.base_path);
+        }
+
+        // Handle flickr images.
+        if ($(child).attr("class").match("flickr-photo-img") ||
+          $(child).attr("class").match("flickr-photoset-img")) {
+          href = $(child).attr("src").replace("_s", "").replace("_t", "").replace("_m", "").replace("_b", "");
+          if (rel_type != "lightbox_ungrouped") {
+            rel = rel_type + "[flickr]";
+            if ($(child).parents("div.block-flickr").attr("class")) {
+              id = $(child).parents("div.block-flickr").attr("id");
+              rel = rel_type + "["+ id +"]";
+            }
+          }
+        }
+
+        // Handle "image-img_assist_custom" images.
+        else if ($(child).attr("class").match("image-img_assist_custom")) {
+          // Image assist uses "+" signs for spaces which doesn't work for
+          // normal links.
+          orig_href = orig_href.replace(/\+/, " ");
+          href = orig_href;
+        }
+
+        // Handle "inline" images.
+        else if ($(child).attr("class").match("inline")) {
+          href = orig_href;
+        }
+
+        // Handle gallery2 block images.
+        else if ($(child).attr("class").match("ImageFrame_image")) {
+          var thumb_id = parse_url(href, "g2_itemId");
+          var new_id = parse_url(orig_href, "g2_itemId");
+          if (new_id && thumb_id) {
+            var g2pattern = new RegExp("g2_itemId="+thumb_id);
+            var replacement = "g2_itemId="+ new_id;
+            href = href.replace(g2pattern, replacement);
+          }
+        }
+
+
+        // Set the href attribute.
+        else if (settings.image_node_sizes != '()') {
+          href = $(child).attr("src").replace(new RegExp(settings.image_node_sizes), ((settings.display_image_size === "")?settings.display_image_size:"."+ settings.display_image_size)).replace(/(image\/view\/\d+)(\/\w*)/, ((settings.display_image_size === "")?"$1/_original":"$1/"+ settings.display_image_size));
+          if (rel_type != "lightbox_ungrouped") {
+            rel = rel_type + "[node_images]";
+            if ($(child).parents("div.block-image").attr("class")) {
+              id = $(child).parents("div.block-image").attr("id");
+              rel = rel_type + "["+ id +"]";
+            }
+          }
+        }
+
+        // Modify the image url.
+        var img_title = $(child).attr("title");
+        if (!img_title) {
+          img_title = $(this).attr("title");
+          $(child).attr({title: img_title});
+        }
+        if (!custom_class) {
+          $(this).attr({
+            rel: rel,
+            title: alt + "<br /><a href=\"" + orig_href + "\" id=\"node_link_text\" "+ link_target +" >"+ link_text + "</a>",
+            href: href
+          });
+        }
+        else {
+          $(this).attr({
+            rel: rel,
+            title: alt,
+            href: orig_href
+          });
+        }
+      }
+    }
+
+  });
+
+}
+
 if (Drupal.jsEnabled) {
   $(document).ready(function lightbox2_image_nodes() {
 
@@ -21,136 +155,4 @@ if (Drupal.jsEnabled) {
 
     }
   });
-
-  function lightbox2_init_triggers(classes, rel_type, custom_class) {
-    var settings = Drupal.settings.lightbox2;
-
-    $("a["+classes+"]").each(function(i) {
-
-      if ((!settings.disable_for_gallery_lists && !settings.disable_for_acidfree_gallery_lists) || (!$(this).parents(".galleries").length && !$(this).parents(".acidfree-folder").length && !$(this).parents(".acidfree-list").length) || ($(this).parents(".galleries").length && !settings.disable_for_gallery_lists) || (($(this).parents(".acidfree-folder").length || $(this).parents(".acidfree-list").length) && !settings.disable_for_acidfree_gallery_lists)) {
-
-        var child = $(this).children(classes);
-
-        // Ensure the child has a class attribute we can work with.
-        if ($(child).attr("class")) {
-
-          // Set the alt text.
-          var alt = $(child).attr("alt");
-          if (!alt) {
-            alt = "";
-          }
-
-          // Set the image node link text.
-          var link_text = settings.node_link_text;
-          var link_target  = "";
-          if (settings.node_link_target != 0) {
-            link_target = 'target="'+ settings.node_link_target +'"';
-          }
-
-          // Set the rel attribute.
-          var rel = "lightbox";
-          if (rel_type != "lightbox_ungrouped") {
-            rel = rel_type + "[" + $(child).attr("class") + "]";
-          }
-
-          // Set the basic href attribute - need to ensure there's no language
-          // string (e.g. /en) prepended to the URL.
-          var href = $(child).attr("src");
-          var orig_href = $(this).attr("href");
-          var pattern = new RegExp(settings.file_path);
-          if (orig_href.match(pattern)) {
-            var lang_pattern = new RegExp(settings.base_path + "\\w\\w\\/");
-            orig_href = orig_href.replace(lang_pattern, settings.base_path);
-          }
-
-          // Handle flickr images.
-          if ($(child).attr("class").match("flickr-photo-img")
-            || $(child).attr("class").match("flickr-photoset-img")) {
-            href = $(child).attr("src").replace("_s", "").replace("_t", "").replace("_m", "").replace("_b", "");
-            if (rel_type != "lightbox_ungrouped") {
-              rel = rel_type + "[flickr]";
-              if ($(child).parents("div.block-flickr").attr("class")) {
-                var id = $(child).parents("div.block-flickr").attr("id");
-                rel = rel_type + "["+ id +"]";
-              }
-            }
-          }
-
-          // Handle "image-img_assist_custom" images.
-          else if ($(child).attr("class").match("image-img_assist_custom")) {
-            // Image assist uses "+" signs for spaces which doesn't work for
-            // normal links.
-            orig_href = orig_href.replace(/\+/, " ");
-            href = orig_href;
-          }
-
-          // Handle "inline" images.
-          else if ($(child).attr("class").match("inline")) {
-            href = orig_href;
-          }
-
-          // Handle gallery2 block images.
-          else if ($(child).attr("class").match("ImageFrame_image")) {
-            var thumb_id = parse_url(href, "g2_itemId");
-            var new_id = parse_url(orig_href, "g2_itemId");
-            if (new_id && thumb_id) {
-              var pattern = new RegExp("g2_itemId="+thumb_id);
-              var replacement = "g2_itemId="+ new_id;
-              var href = href.replace(pattern, replacement);
-            }
-          }
-
-
-          // Set the href attribute.
-          else if (settings.image_node_sizes != '()') {
-            href = $(child).attr("src").replace(new RegExp(settings.image_node_sizes), ((settings.display_image_size == "")?settings.display_image_size:"."+ settings.display_image_size)).replace(/(image\/view\/\d+)(\/\w*)/, ((settings.display_image_size == "")?"$1/_original":"$1/"+ settings.display_image_size));
-            if (rel_type != "lightbox_ungrouped") {
-              rel = rel_type + "[node_images]";
-              if ($(child).parents("div.block-image").attr("class")) {
-                var id = $(child).parents("div.block-image").attr("id");
-                rel = rel_type + "["+ id +"]";
-              }
-            }
-          }
-
-          // Modify the image url.
-          var img_title = $(child).attr("title");
-          if (!img_title) {
-            img_title = $(this).attr("title");
-            $(child).attr({title: img_title});
-          }
-          if (!custom_class) {
-            $(this).attr({
-              rel: rel,
-              title: alt + "<br /><a href=\"" + orig_href + "\" id=\"node_link_text\" "+ link_target +" >"+ link_text + "</a>",
-              href: href
-            });
-          }
-          else {
-            $(this).attr({
-              rel: rel,
-              title: alt,
-              href: orig_href
-            });
-          }
-        }
-      }
-
-    });
-
-  }
-
-  function parse_url(url, param) {
-    param = param.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-    url = url.replace(/&amp;/, "&");
-    var regexS = "[\\?&]"+param+"=([^&#]*)";
-    var regex = new RegExp(regexS);
-    var results = regex.exec(url);
-    if (results == null) {
-      return "";
-    }
-    else {
-      return results[1];
-    }
-  }
 }
--- js/lightbox_lite.js
+++ js/lightbox_lite.js
@@ -1,7 +1,7 @@
 /* $Id$ */
 
 /**
- * Lightbox JS: Fullsize Image Overlays 
+ * Lightbox JS: Fullsize Image Overlays
  * by Lokesh Dhakar - http://www.huddletogether.com
  *
  * For more information on this script, visit:
@@ -10,11 +10,11 @@
  * This script is distributed via Drupal.org with permission from Lokesh Dhakar.
  * Under GPL license.
  *    Mailto: bugzie@gmail.com
- * 
+ *
  * Table of Contents
  * -----------------
  * Configuration
- * 
+ *
  * Functions
  * - getPageScroll()
  * - getPageSize()
@@ -24,7 +24,7 @@
  * - showLightbox()
  * - hideLightbox()
  * - initLightbox()
- * 
+ *
  */
 
 
@@ -54,7 +54,7 @@ function getPageScroll() {
     xScroll = document.body.scrollLeft;
   }
 
-  arrayPageScroll = new Array(xScroll, yScroll);
+  arrayPageScroll = [xScroll, yScroll];
   return arrayPageScroll;
 }
 
@@ -65,10 +65,10 @@ function getPageScroll() {
 // Core code from - quirksmode.org
 // Edit for Firefox by pHaez
 function getPageSize() {
- 
+
   var xScroll, yScroll;
- 
-  if (window.innerHeight && window.scrollMaxY) { 
+
+  if (window.innerHeight && window.scrollMaxY) {
     xScroll = window.innerWidth + window.scrollMaxX;
     yScroll = window.innerHeight + window.scrollMaxY;
   // all but Explorer Mac
@@ -82,7 +82,7 @@ function getPageSize() {
     xScroll = document.body.offsetWidth;
     yScroll = document.body.offsetHeight;
   }
- 
+
   var windowWidth, windowHeight;
   if (self.innerHeight) { // all except Explorer
     if (document.documentElement.clientHeight) {
@@ -100,18 +100,18 @@ function getPageSize() {
   else if (document.body) { // other Explorers
     windowWidth = document.body.clientWidth;
     windowHeight = document.body.clientHeight;
-  } 
- 
+  }
+
   // for small pages with total height less then height of the viewport
   if (yScroll < windowHeight) {
     pageHeight = windowHeight;
   }
-  else { 
+  else {
     pageHeight = yScroll;
   }
 
   // for small pages with total width less then width of the viewport
-  if (xScroll < windowWidth) { 
+  if (xScroll < windowWidth) {
     pageWidth = xScroll;
   }
   else {
@@ -119,7 +119,7 @@ function getPageSize() {
   }
 
 
-  arrayPageSize = new Array(pageWidth, pageHeight, windowWidth, windowHeight);
+  arrayPageSize = [pageWidth, pageHeight, windowWidth, windowHeight];
   return arrayPageSize;
 }
 
@@ -127,135 +127,9 @@ function getPageSize() {
 // pause(numberMillis)
 function pause(ms) {
   var date = new Date();
-  curDate = null;
-  do { var curDate = new Date(); }
+  var curDate = null;
+  do { curDate = new Date(); }
   while (curDate - date < ms);
-}
-
-// getKey(key)
-// Gets keycode. If 'x' is pressed then it hides the lightbox.
-function getKey(e) {
-  if (e === null) { // ie
-    keycode = event.keyCode;
-    escapeKey = 27;
-  }
-  else { // mozilla
-    keycode = e.which;
-    escapeKey = e.DOM_VK_ESCAPE;
-  }
-  key = String.fromCharCode(keycode).toLowerCase();
- 
-  if (key == 'x' || key == 'c' || keycode == escapeKey) { hideLightbox(); }
-}
-
-
-// listenKey()
-function listenKey () { document.onkeypress = getKey; }
- 
-
-// showLightbox()
-// Preloads images. Pleaces new image in lightbox then centers and displays.
-function showLightbox(objLink) {
-  var settings = Drupal.settings.lightbox2;
-  // prep objects
-  var objOverlay = document.getElementById('overlay');
-  var objLightbox = document.getElementById('lightbox');
-  var objCaption = document.getElementById('lightboxCaption');
-  var objImage = document.getElementById('lightboxImage');
-  var objLoadingImage = document.getElementById('loadingImage');
-  var objLightboxDetails = document.getElementById('lightboxDetails');
-
-  var arrayPageSize = getPageSize();
-  var arrayPageScroll = getPageScroll();
-
-  // set height of Overlay to take up whole page and show
-  objOverlay.style.height = (arrayPageSize[1] + 'px');
-  objOverlay.style.display = 'block';
-  objOverlay.style.opacity = settings.overlay_opacity;
-
-  // preload image
-  imgPreload = new Image();
-  imgPreload.onerror = function() { imgLoadingError(this, objImage, objLink) };
-
-  imgPreload.onload = function() {
-    objImage.src = objLink.href;
-
-    // center lightbox and make sure that the top and left values are not 
-    // negative and the image placed outside the viewport
-    var lightboxTop = arrayPageScroll[1] + ((arrayPageSize[3] - 35 - imgPreload.height) / 2);
-    var lightboxLeft = ((arrayPageSize[0] - 20 - imgPreload.width) / 2);
-  
-    objLightbox.style.top = (lightboxTop < 0) ? "0px" : lightboxTop + "px";
-    objLightbox.style.left = (lightboxLeft < 0) ? "0px" : lightboxLeft + "px";
-
-
-    //objLightboxDetails.style.width = imgPreload.width + 'px';
-    objLightbox.style.width = imgPreload.width + 'px';
-  
-    if (objLink.getAttribute('title')) {
-      objCaption.style.display = 'block';
-      //objCaption.style.width = imgPreload.width + 'px';
-      objCaption.innerHTML = objLink.getAttribute('title');
-    }
-    else {
-      objCaption.style.display = 'none';
-    }
-  
-    // A small pause between the image loading and displaying is required with 
-    // IE,  this prevents the previous image displaying for a short burst 
-    // causing flicker.
-    if (navigator.appVersion.indexOf("MSIE") != -1) {
-      pause(250);
-    } 
-
-    if (objLoadingImage) { objLoadingImage.style.display = 'none'; }
-
-    // Hide select boxes as they will 'peek' through the image in IE
-    selects = document.getElementsByTagName("select");
-    for (i = 0; i != selects.length; i++) {
-      if (selects[i].style.display != "none") {
-        selects[i].style.visibility = "hidden";
-      }
-    }
-
-    // Hide flash objects as they will 'peek' through the image in IE
-    embed = document.getElementsByTagName("embed");
-    for (i = 0; i != embed.length; i++) {
-      if (embed[i].style.display != "none") {
-        embed[i].style.visibility = "hidden";
-      }
-    }
-    objects = document.getElementsByTagName("object");
-    for (i = 0; i != objects.length; i++) {
-      if (objects[i].style.display != "none") {
-        objects[i].style.visibility = "hidden";
-      }
-    }
-
-    objLightbox.style.display = 'block';
-
-    // After image is loaded, update the overlay height as the new image might
-    // have increased the overall page height.
-    arrayPageSize = getPageSize();
-    objOverlay.style.height = (arrayPageSize[1] + 'px');
-
-    // Check for 'x' keypress
-    listenKey();
-
-    return false;
-  };
-
-  imgPreload.src = objLink.href;
- 
-}
-
-
-
-function imgLoadingError(image, objImage, objLink) {
-  var settings = Drupal.settings.lightbox2;
-  image.src = settings.default_image;
-  objImage.src = settings.default_image;
-  objLink.href = settings.default_image;
 }
 
 
@@ -296,16 +170,143 @@ function hideLightbox() {
 }
 
 
+// getKey(key)
+// Gets keycode. If 'x' is pressed then it hides the lightbox.
+function getKey(e) {
+  if (e === null) { // ie
+    keycode = event.keyCode;
+    escapeKey = 27;
+  }
+  else { // mozilla
+    keycode = e.which;
+    escapeKey = e.DOM_VK_ESCAPE;
+  }
+  key = String.fromCharCode(keycode).toLowerCase();
+
+  if (key == 'x' || key == 'c' || keycode == escapeKey) { hideLightbox(); }
+}
+
+
+// listenKey()
+function listenKey () { document.onkeypress = getKey; }
+
+
+function imgLoadingError(image, objImage, objLink) {
+  var settings = Drupal.settings.lightbox2;
+  image.src = settings.default_image;
+  objImage.src = settings.default_image;
+  objLink.href = settings.default_image;
+}
+
+
+// showLightbox()
+// Preloads images. Pleaces new image in lightbox then centers and displays.
+function showLightbox(objLink) {
+  var settings = Drupal.settings.lightbox2;
+  // prep objects
+  var objOverlay = document.getElementById('overlay');
+  var objLightbox = document.getElementById('lightbox');
+  var objCaption = document.getElementById('lightboxCaption');
+  var objImage = document.getElementById('lightboxImage');
+  var objLoadingImage = document.getElementById('loadingImage');
+  var objLightboxDetails = document.getElementById('lightboxDetails');
+
+  var arrayPageSize = getPageSize();
+  var arrayPageScroll = getPageScroll();
+
+  // set height of Overlay to take up whole page and show
+  objOverlay.style.height = (arrayPageSize[1] + 'px');
+  objOverlay.style.display = 'block';
+  objOverlay.style.opacity = settings.overlay_opacity;
+
+  // preload image
+  imgPreload = new Image();
+  imgPreload.onerror = function() { imgLoadingError(this, objImage, objLink); };
+
+  imgPreload.onload = function() {
+    objImage.src = objLink.href;
+
+    // center lightbox and make sure that the top and left values are not
+    // negative and the image placed outside the viewport
+    var lightboxTop = arrayPageScroll[1] + ((arrayPageSize[3] - 35 - imgPreload.height) / 2);
+    var lightboxLeft = ((arrayPageSize[0] - 20 - imgPreload.width) / 2);
+
+    objLightbox.style.top = (lightboxTop < 0) ? "0px" : lightboxTop + "px";
+    objLightbox.style.left = (lightboxLeft < 0) ? "0px" : lightboxLeft + "px";
+
+
+    //objLightboxDetails.style.width = imgPreload.width + 'px';
+    objLightbox.style.width = imgPreload.width + 'px';
+
+    if (objLink.getAttribute('title')) {
+      objCaption.style.display = 'block';
+      //objCaption.style.width = imgPreload.width + 'px';
+      objCaption.innerHTML = objLink.getAttribute('title');
+    }
+    else {
+      objCaption.style.display = 'none';
+    }
+
+    // A small pause between the image loading and displaying is required with
+    // IE,  this prevents the previous image displaying for a short burst
+    // causing flicker.
+    if (navigator.appVersion.indexOf("MSIE") != -1) {
+      pause(250);
+    }
+
+    if (objLoadingImage) { objLoadingImage.style.display = 'none'; }
+
+    // Hide select boxes as they will 'peek' through the image in IE
+    selects = document.getElementsByTagName("select");
+    for (i = 0; i != selects.length; i++) {
+      if (selects[i].style.display != "none") {
+        selects[i].style.visibility = "hidden";
+      }
+    }
+
+    // Hide flash objects as they will 'peek' through the image in IE
+    embed = document.getElementsByTagName("embed");
+    for (i = 0; i != embed.length; i++) {
+      if (embed[i].style.display != "none") {
+        embed[i].style.visibility = "hidden";
+      }
+    }
+    objects = document.getElementsByTagName("object");
+    for (i = 0; i != objects.length; i++) {
+      if (objects[i].style.display != "none") {
+        objects[i].style.visibility = "hidden";
+      }
+    }
+
+    objLightbox.style.display = 'block';
+
+    // After image is loaded, update the overlay height as the new image might
+    // have increased the overall page height.
+    arrayPageSize = getPageSize();
+    objOverlay.style.height = (arrayPageSize[1] + 'px');
+
+    // Check for 'x' keypress
+    listenKey();
+
+    return false;
+  };
+
+  imgPreload.src = objLink.href;
+
+}
+
+
+
 
 
 // initLightbox()
-// Function runs on window load, going through link tags looking for 
+// Function runs on window load, going through link tags looking for
 // rel="lightbox".  These links receive onclick events that enable the lightbox
 // display for their targets.  The function also inserts html markup at the top
-// of the page which will be used as a container for the overlay pattern and 
+// of the page which will be used as a container for the overlay pattern and
 // the inline image.
 function initLightbox() {
- 
+
   if (!document.getElementsByTagName) { return; }
   var anchors = document.getElementsByTagName("a");
 
@@ -319,14 +320,14 @@ function initLightbox() {
     }
   }
 
-  // the rest of this code inserts html at the top of the page that looks like 
+  // the rest of this code inserts html at the top of the page that looks like
   // this:
   // <div id="overlay">
   //  <a href="#" onclick="hideLightbox(); return false;"><img id="loadingImage" /></a>
   // </div>
   // <div id="lightbox">
   //  <a href="#" onclick="hideLightbox(); return false;" title="Click anywhere to close image">
-  //   <img id="closeButton" />  
+  //   <img id="closeButton" />
   //   <img id="lightboxImage" />
   //  </a>
   //  <div id="lightboxDetails">
@@ -334,9 +335,9 @@ function initLightbox() {
   //   <div id="keyboardMsg"></div>
   //  </div>
   // </div>
-  
+
   var objBody = document.getElementsByTagName("body").item(0);
- 
+
   // create overlay div and hardcode some functional styles (aesthetic styles are in CSS file)
   var objOverlay = document.createElement("div");
   objOverlay.setAttribute('id', 'overlay');
@@ -348,7 +349,7 @@ function initLightbox() {
   objOverlay.style.zIndex = '90';
   objOverlay.style.width = '100%';
   objBody.insertBefore(objOverlay, objBody.firstChild);
- 
+
   var arrayPageSize = getPageSize();
   var arrayPageScroll = getPageScroll();
 
@@ -362,9 +363,9 @@ function initLightbox() {
   objLightbox.setAttribute('id', 'lightbox');
   objLightbox.style.display = 'none';
   objLightbox.style.position = 'absolute';
-  objLightbox.style.zIndex = '100'; 
+  objLightbox.style.zIndex = '100';
   objBody.insertBefore(objLightbox, objOverlay.nextSibling);
- 
+
   // create link
   var objLink = document.createElement("a");
   objLink.setAttribute('href', '#');
@@ -381,7 +382,7 @@ function initLightbox() {
   var objImage = document.createElement("img");
   objImage.setAttribute('id', 'lightboxImage');
   objLink.appendChild(objImage);
- 
+
   // create details div, a container for the caption and keyboard message
   var objLightboxDetails = document.createElement("div");
   objLightboxDetails.setAttribute('id', 'lightboxDetails');
