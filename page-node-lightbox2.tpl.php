e11bd11ea9f50eec922a27785f977d6eb3398cfc
Stella Power <stella@66894.no-reply.drupal.org>
2008-06-11 22:16:56 +0000

    added in missing @file block

--- page-node-lightbox2.tpl.php
+++ page-node-lightbox2.tpl.php
@@ -1,5 +1,11 @@
 <?php
 // $Id$
+
+/**
+ * @file
+ * Template file for displaying the node content, associated with an image, in
+ * the lightbox.  It displays it without any sidebars, etc.
+ */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language ?>" lang="<?php print $language ?>">
