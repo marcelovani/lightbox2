9c100a2269b0bdd96cb68c5432a2f9a950b01fa6
Stella Power <stella@66894.no-reply.drupal.org>
2008-08-11 10:10:37 +0000

    Fixed typo.

--- js/lightbox.js
+++ js/lightbox.js
@@ -1045,10 +1045,10 @@ var Lightbox = {
 
 };
 
-if (!Array.indexOf){
-  Array.prototype.indexOf = function(obj){
-    for (var i = 0; i < this.length; i++){
-      if (this[i] == obj){
+if (!Array.indexOf) {
+  Array.prototype.indexOf = function(obj) {
+    for (var i = 0; i < this.length; i++) {
+      if (this[i] == obj) {
         return i;
       }
     }
