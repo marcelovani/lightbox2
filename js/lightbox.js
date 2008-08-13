715dffaea6cbd85c2dd2436933e74f42397e3b58
Stella Power <stella@66894.no-reply.drupal.org>
2008-08-13 13:52:30 +0000

    #294602: added workaround for IE6 not supporting the min-width CSS attribute.

--- js/lightbox.js
+++ js/lightbox.js
@@ -25,6 +25,7 @@ var Lightbox = {
   resizeSpeed: 'normal', // Controls the speed of the lightbox resizing animation.
   fadeInSpeed: 'normal', // Controls the speed of the image and overlay appearance.
   slideDownSpeed: 'slow', // Controls the speed of the image details appearance.
+  minWidth: 240,
   borderSize : 10,
   boxColor : 'fff',
   fontColor : '000',
@@ -481,6 +482,8 @@ var Lightbox = {
 
   // resizeContainer()
   resizeContainer: function(imgWidth, imgHeight) {
+
+    imgWidth = (imgWidth < Lightbox.minWidth ? Lightbox.minWidth : imgWidth);
 
     this.widthCurrent = $('#outerImageContainer').width();
     this.heightCurrent = $('#outerImageContainer').height();
